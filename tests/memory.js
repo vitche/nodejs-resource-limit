5c08011fb5aae9f36b63468f863d8ef0b40e970a
Vitche Research Team Developer <developer@vitche.com>
2020-01-13 12:45:57 -0500

    1. Initial working version.

---
+++ tests/memory.js
@@ -0,0 +1,18 @@
+let ResourceLimit = require('../main');
+
+new ResourceLimit()
+    .memory()
+    .moreThan(2570000)
+    .terminate();
+
+new ResourceLimit()
+	.periodical(500)
+    .memory()
+    .moreThan(100000)
+    .nothing();
+
+new ResourceLimit()
+	.periodical(2000)
+    .memory()
+    .moreThan(2700000)
+    .terminate();
