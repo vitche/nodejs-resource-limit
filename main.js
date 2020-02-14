a906736c402f47c05862e4880e8025f5877ec713
Vitche Research Team Developer <developer@vitche.com>
2020-02-14 11:10:35 -0500

    1. A new empty resource check for event listenters count.

--- main.js
+++ main.js
@@ -1,47 +1,58 @@
 module.exports = function (argument) {
-	// Instance magic
-	let self = this;
-	// Ability to perform periodical checks
-	this.periodical = (interval) => {
-		this.interval = setInterval(() => {
-			if (undefined !== this.check && 
-				undefined !== this.checkArguments &&
-				undefined !== this.action && 
-				undefined !== this.actionArguments) {
-				let result = this.check.apply(this, this.checkArguments);
-				this.action.apply(result, this.actionArguments);
-			}
-		}, interval);
-		return this;
-	}
-	// Possible actions to happen when the trigger condition is met
-	let triggeredAction = function(result) {
-		self.actionArguments = arguments;
-		// Do nothing
-		this.nothing = function nothing(boundResult) {
-			self.action = nothing;
-		}
-		// Terminate the current process
-		this.terminate = function terminate(boundResult) {
-			self.action = terminate;
-			if (result || boundResult) {
-				process.exit();
-			}
-		};
-		return this;
-	};
-	// Memory-related limits
-	this.memory = function() {
-		return {
-			// A limit to check that no more than "allowedAmount" memory is used
-			moreThan: function moreThan(allowedAmount) {
-				self.checkArguments = arguments;
-				self.check = moreThan;
-				const amount = process.memoryUsage().heapUsed;
-				const result =  allowedAmount < amount;
-				console.log("Resource limit, memory: " + allowedAmount  + "?<" + amount);
-				return triggeredAction(result);
-			}
-		}
-	}
+    // Instance magic
+    let self = this;
+    // Ability to perform periodical checks
+    this.periodical = (interval) => {
+        this.interval = setInterval(() => {
+            if (undefined !== this.check &&
+                undefined !== this.checkArguments &&
+                undefined !== this.action &&
+                undefined !== this.actionArguments) {
+                let result = this.check.apply(this, this.checkArguments);
+                this.action.apply(result, this.actionArguments);
+            }
+        }, interval);
+        return this;
+    };
+    // Possible actions to happen when the trigger condition is met
+    let triggeredAction = function (result) {
+        self.actionArguments = arguments;
+        // Do nothing
+        this.nothing = function nothing(boundResult) {
+            self.action = nothing;
+        };
+        // Terminate the current process
+        this.terminate = function terminate(boundResult) {
+            self.action = terminate;
+            if (result || boundResult) {
+                process.exit();
+            }
+        };
+        return this;
+    };
+    // Memory-related limits
+    this.memory = function () {
+        return {
+            // A limit to check that no more than "allowedAmount" memory is used
+            moreThan: function moreThan(allowedAmount) {
+                self.checkArguments = arguments;
+                self.check = moreThan;
+                const amount = process.memoryUsage().heapUsed;
+                const result = allowedAmount < amount;
+                console.log("Resource limit, memory: " + allowedAmount + "?<" + amount);
+                return triggeredAction(result);
+            }
+        }
+    };
+    // Event-related limits
+    this.events = function () {
+        return {
+            moreListenersThan: function moreListenersThan(allowedAmount) {
+                self.checkArguments = arguments;
+                self.check = moreListenersThan;
+                const result = true;
+                return triggeredAction(result);
+            }
+        }
+    };
 };
--- tests/memory.js
+++ tests/memory.js
@@ -1,18 +1,23 @@
 let ResourceLimit = require('../main');
 
 new ResourceLimit()
+    .events()
+    .moreListenersThan(10)
+    .terminate();
+
+new ResourceLimit()
     .memory()
     .moreThan(2570000)
     .terminate();
 
 new ResourceLimit()
-	.periodical(500)
+    .periodical(500)
     .memory()
     .moreThan(100000)
     .nothing();
 
 new ResourceLimit()
-	.periodical(2000)
+    .periodical(2000)
     .memory()
     .moreThan(2700000)
     .terminate();
